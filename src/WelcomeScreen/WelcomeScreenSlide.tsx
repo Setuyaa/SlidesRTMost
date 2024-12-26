629fc28869d91e59388d0394f4ee70cce7bbfe98
anastasia <adeshchits@rtin3-devops-t-mst>
2024-12-26 16:33:50 +0300

    fixed content

--- src/WelcomeScreen/WelcomeScreenSlide.tsx
+++ src/WelcomeScreen/WelcomeScreenSlide.tsx
@@ -8,17 +8,19 @@ import '../css/WelcomeScreenSlide.scss';
 import '../Button/index';
 
 type WelcomeScreenSlideProps = {
-    title: string| React.ReactElement;
+    title: string | React.ReactElement;
+    title2?: string | React.ReactElement;
     subtitle: string | React.ReactElement;
     image: React.ReactNode;
     isMain?: boolean;
     button?: React.ReactElement;
 };
 const formatText = (text: string): string => {
-    return text.replace(/{br}/g, '\n');
-    };
+    return text.replace(/{br}/g, '&nbsp;');
+};
 const WelcomeScreenSlide = ({
     title,
+    title2,
     subtitle,
     image,
     isMain,
@@ -32,14 +34,19 @@ const WelcomeScreenSlide = ({
             },
         )}
     >
+                <div className='WelcomeScreenSlide__ti'>
         <div className='WelcomeScreenSlide__image'>
             {image}
         </div>
-        <div className='WelcomeScreenSlide__title'>
-            {typeof title === 'string' ? <pre>{formatText(title)}</pre> : title}
+            <div className='WelcomeScreenSlide__title'>
+                {title}
+            </div>
+            <div className='WelcomeScreenSlide__title'>
+                {title2}
+            </div>
         </div>
         <div className='WelcomeScreenSlide__subtitle'>
-            {typeof subtitle === 'string' ? <pre>{formatText(subtitle)} </pre> : subtitle}
+            {subtitle}
         </div>
         <div className='WelcomeScreenSlide__button'>
             {button}
